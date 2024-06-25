e455ccf1c1387fa7007ff34abe7b6887f8739635
Monica Sharma <msharma3681@yahoo.com>
2024-06-25 12:26:59 -0400

    First Commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,74 +1,84 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
+import org.yearup.data.mysql.MySqlCategoryDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
 import java.util.List;
 
-        @RestController                            // add the annotations to make this a REST controller
-        @RequestMapping("/categories")        // add the annotation to make this controller the endpoint for the following url
-                                                 // http://localhost:8080/categories
-        @CrossOrigin
-        public class CategoriesController
-        {                                          // add annotation to allow cross site origin requests
+@RestController                            // add the annotations to make this a REST controller
+@RequestMapping("/categories")        // add the annotation to make this controller the endpoint for the following url
+// http://localhost:8080/categories
+@CrossOrigin
+public class CategoriesController {                                          // add annotation to allow cross site origin requests
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
 
-            @Autowired
-            public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
-                this.categoryDao = categoryDao;
-                this.productDao = productDao;
-            }
-            // create an Autowired controller to inject the categoryDao and ProductDao
+    @Autowired // create an Autowired controller to inject the categoryDao and ProductDao
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
 
     // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+
+
+    @GetMapping("")  // add the appropriate annotation for a get action
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Category> getCategories() {
+        try {
+            return categoryDao.getAllCategories();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
     }
+
+
 
     // add the appropriate annotation for a get action
     public Category getById(@PathVariable int id)
     {
         // get the category by id
         return null;
-    }
-
-    // the url to return all products in category 1 would look like this
-    // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        // get a list of product by categoryId
-        return null;
-    }
-
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
-    public Category addCategory(@RequestBody Category category)
-    {
-        // insert the category
-        return null;
-    }
-
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
-        // update the category by id
-    }
-
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    public void deleteCategory(@PathVariable int id)
-    {
-        // delete the category by id
-    }
+  }
+//
+//    // the url to return all products in category 1 would look like this
+//    // https://localhost:8080/categories/1/products
+//    @GetMapping("{categoryId}/products")
+//    public List<Product> getProductsById(@PathVariable int categoryId)
+//    {
+//        // get a list of product by categoryId
+//        return null;
+//    }
+//
+//    // add annotation to call this method for a POST action
+//    // add annotation to ensure that only an ADMIN can call this function
+//    public Category addCategory(@RequestBody Category category)
+//    {
+//        // insert the category
+//        return null;
+//    }
+//
+//    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+//    // add annotation to ensure that only an ADMIN can call this function
+//    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+//    {
+//        // update the category by id
+//    }
+//
+//
+//    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+//    // add annotation to ensure that only an ADMIN can call this function
+//    public void deleteCategory(@PathVariable int id)
+//    {
+//        // delete the category by id
+//    }
 }
