4443c1280d36ae3770e7aa4556bf0d512e222be9
Monica Sharma <msharma3681@yahoo.com>
2024-06-25 13:49:02 -0400

    First Commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -10,6 +10,7 @@ import org.yearup.data.mysql.MySqlCategoryDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController                            // add the annotations to make this a REST controller
@@ -17,53 +18,68 @@ import java.util.List;
 // http://localhost:8080/categories
 @CrossOrigin
 public class CategoriesController {                                          // add annotation to allow cross site origin requests
-    private CategoryDao categoryDao;
-    private ProductDao productDao;
+private CategoryDao categoryDao;
+private ProductDao productDao;
 
 
-    @Autowired // create an Autowired controller to inject the categoryDao and ProductDao
-    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
-        this.categoryDao = categoryDao;
-        this.productDao = productDao;
-    }
-
-
-    // add the appropriate annotation for a get action
-
-
-    @GetMapping("")  // add the appropriate annotation for a get action
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<Category> getCategories() {
-        try {
-            return categoryDao.getAllCategories();
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
-        }
-    }
-
-
-    // add the appropriate annotation for a get action  // get the category by id
-public Category getbycategoryID(@PathVariable int id){
-
-try {
-    var category = categoryDao.getById(id);
-
-    if (category == null)
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-    return category;
+@Autowired // create an Autowired controller to inject the categoryDao and ProductDao
+public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+    this.categoryDao = categoryDao;
+    this.productDao = productDao;
 }
-catch (Exception ex) {
-    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+
+
+// add the appropriate annotation for a get action
+
+
+@GetMapping("")  // add the appropriate annotation for a get action
+@ResponseStatus(value = HttpStatus.OK)
+public List<Category> getCategories() {
+    try {
+        return categoryDao.getAllCategories();
+    } catch (Exception ex) {
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+    }
 }
+
+
+// add the appropriate annotation for a get action  // get the category by id
+public Category getbycategoryID(@PathVariable int id) {
+
+    try {
+        var category = categoryDao.getById(id);
+
+        if (category == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
+    } catch (Exception ex) {
+        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+    }
 }                                                                   // get the category by id
 
+
+// the url to return all products in category 1 would look like this
+// https://localhost:8080/categories/1/products
+
+@GetMapping("/categories/{categoryId}/products")
+public List<Product> getProductsInCategory(@PathVariable Long categoryId) {
+    if (categoryId == 1) {
+        return Arrays.asList(new Product());
+    } else {
+        return null;
+    }
 }
-//
-//    // the url to return all products in category 1 would look like this
-//    // https://localhost:8080/categories/1/products
-//    @GetMapping("{categoryId}/products")
-//    public List<Product> getProductsById(@PathVariable int categoryId)
+}
+
+
+
+
+
+
+
+
+
 //    {
 //        // get a list of product by categoryId
 //        return null;
