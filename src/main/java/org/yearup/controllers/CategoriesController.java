9fd438bca788aabf0b52838b9dacf06b69360821
Monica Sharma <msharma3681@yahoo.com>
2024-06-25 12:56:54 -0400

    First Commit

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -42,13 +42,23 @@ public class CategoriesController {                                          // 
     }
 
 
+    // add the appropriate annotation for a get action  // get the category by id
+public Category getbycategoryID(@PathVariable int id){
 
-    // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
-        // get the category by id
-        return null;
-  }
+try {
+    var category = categoryDao.getById(id);
+
+    if (category == null)
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    return category;
+}
+catch (Exception ex) {
+    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+}
+}                                                                   // get the category by id
+
+}
 //
 //    // the url to return all products in category 1 would look like this
 //    // https://localhost:8080/categories/1/products
@@ -81,4 +91,4 @@ public class CategoriesController {                                          // 
 //    {
 //        // delete the category by id
 //    }
-}
+
